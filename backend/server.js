0c9dd49a9dcd4c49db4a13f6259639d29475bd15
michalwojcieszek <mwoj772@gmail.com>
2024-03-27 09:58:42 +0100

    render deploy v4

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,21 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/trips", tripsRoutes);
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // Any route that is not API will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
